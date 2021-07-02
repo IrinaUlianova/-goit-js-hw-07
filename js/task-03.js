fdfd1d609c3d61a115d9f3bf30e4f92d2ab9d717
Irina Ulianova <ulianovira.89@gmail.com>
2021-07-02 20:47:14 +0300

    add 3 task

--- js/task-03.js
+++ js/task-03.js
@@ -13,3 +13,21 @@ const images = [
   },
 ];
 console.log(images);
+const boxImageEl = document.querySelector("#gallery");
+console.log(boxImageEl);
+const galleryItems = images.map((images) => {
+  const { url, alt } = images;
+
+  const galleryItemEl = document.createElement(`li`);
+  galleryItemEl.classList.add("gallery__item");
+  return `<li><img src= ${url} alt= ${alt}/></li>`;
+});
+
+console.log(galleryItems);
+boxImageEl.insertAdjacentHTML(`beforeend`, galleryItems);
+
+/*Используй массив объектов images для создания тегов img вложенных в li. 
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.*/
