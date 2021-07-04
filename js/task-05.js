54e3a9290bf40dc9fe1a13fe2f2a69eacf9eae3b
Irina Ulianova <ulianovira.89@gmail.com>
2021-07-04 14:44:56 +0300

    add fifth task

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,13 @@
-// const inputEl = document.querySelector("input#name-input");
-// const nameOutputEl = document.querySelector("#name-output");
-
-// inputEl.addEventListener("input", onInputChange);
-
-// function onInputChange(event) {
-//   event.currentTarget.value.length === 0
-//     ? (nameOutputEl.textContent = "незнакомец")
-//     : (nameOutputEl.textContent = event.currentTarget.value);
-// }
 /*Напиши скрипт который, при наборе текста в инпуте
  input#name-input (событие input), подставляет его текущее
   значение в span#name-output. Если инпут пустой,
    в спане должна отображаться строка 'незнакомец'.*/
+const nameInputRef = document.querySelector(" input#name-input  ");
+console.log(nameInputRef);
+const nameOutputRef = document.querySelector("#name-output");
+console.log(nameOutputRef);
+
+nameInputRef.addEventListener("change", changeInputName);
+function changeInputName() {
+  return (nameOutputRef.textContent = nameInputRef.value);
+}
