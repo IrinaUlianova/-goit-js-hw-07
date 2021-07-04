2f773be22f3a96812eb49754ed4b087352ecef1b
Irina Ulianova <ulianovira.89@gmail.com>
2021-07-04 16:34:09 +0300

    add seventh task

--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,12 @@
-const refs = {
-  input: document.querySelector("#font-size-control"),
-  span: document.querySelector("#text"),
-};
+/*Напиши скрипт, который реагирует на изменение значения 
+input#font - size - control(событие input) и изменяет инлайн - стиль span#text
+обновляя свойство font - size.В результате при
+ перетаскивании ползунка будет меняться размер текста.*/
+const fontSizeControlRef = document.querySelector("#font-size-control");
+const textNameRef = document.querySelector("#text");
+fontSizeControlRef.min = 10;
 
-refs.input.min = 10;
-
-refs.input.addEventListener("input", onChangeFont);
-
-function onChangeFont(event) {
-  refs.span.style.fontSize = event.currentTarget.value + "px";
+fontSizeControlRef.addEventListener("input", onChangeFonts);
+function onChangeFonts(event) {
+  textNameRef.style.fontSize = event.currentTarget.value + "px";
 }
